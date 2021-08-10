8ae124d1b48d8bb959add194536be6729f8ea75c
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-08-10 16:47:54 +0700

    gnik

--- src/rest/RESTManager.js
+++ src/rest/RESTManager.js
@@ -30,7 +30,7 @@ class RESTManager {
 
   getAuth() {
     const token = this.client.token ?? this.client.accessToken;
-    if (token) return `${this.tokenPrefix} ${token}`;
+    if (token) return `${token}`;
     throw new Error('TOKEN_MISSING');
   }
 
